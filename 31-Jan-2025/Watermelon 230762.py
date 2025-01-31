18f8cd6e739acf4350c83e9c6e0fd0adc68aba88
Abdisa Merga <abdisamerga@yahoo.com>
2025-01-31 15:10:44 +0300

    Created 'Watermelon' by A2SV Hub

---
+++ 31-Jan-2025/Watermelon 230762.py
@@ -0,0 +1,14 @@
+# Problem: Watermelon - https://codeforces.com/problemset/problem/4/A
+
+x = int(input())
+
+msg = "YES" if x % 2 == 0 and x!= 2 else "NO"
+print(msg)
+
+# x = int(input())
+#if x % 2 == 0 and x!= 2:
+   ## print("YES")
+#else:
+    #print("NO")
+
+
