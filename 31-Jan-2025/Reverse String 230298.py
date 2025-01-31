218a868fd23c3099da74a0721a2d491feab62001
Abdisa Merga <abdisamerga@yahoo.com>
2025-01-31 12:11:54 +0300

    Created 'Reverse String' by A2SV Hub

---
+++ 31-Jan-2025/Reverse String 230298.py
@@ -0,0 +1,8 @@
+# Problem: Reverse String - https://leetcode.com/problems/reverse-string/description/
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s[:] = s[::-1]
