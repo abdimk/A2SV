9f5b2a15b34177e485675d6ff220ca9bccba4b1e
Abdisa Merga <abdisamerga@yahoo.com>
2025-02-03 18:58:14 +0300

    Created 'Valid Anagram' by A2SV Hub

---
+++ 03-Feb-2025/Valid Anagram 236892.py
@@ -0,0 +1,7 @@
+# Problem: Valid Anagram - https://leetcode.com/problems/valid-anagram/description/
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if sorted(s) == sorted(t):
+            return True
+        return False
