9c118f892dd30669f7533bb40a411b4644ce0cf7
Abdisa Merga <abdisamerga@yahoo.com>
2025-02-03 15:48:38 +0300

    Created 'Missing Number' by A2SV Hub

---
+++ 03-Feb-2025/Missing Number 236336.py
@@ -0,0 +1,9 @@
+# Problem: Missing Number - https://leetcode.com/problems/missing-number/description/
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = sum(nums)
+        m = sum(range(0, len(nums) + 1))
+
+        return m - n
+        
