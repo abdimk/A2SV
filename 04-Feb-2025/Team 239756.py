4df6eba64366c22718d82374d3c89855181085c6
Abdisa Merga <abdisamerga@yahoo.com>
2025-02-04 14:55:55 +0300

    Created 'Team' by A2SV Hub

---
+++ 04-Feb-2025/Team 239756.py
@@ -0,0 +1,13 @@
+# Problem: Team - https://codeforces.com/contest/231/problem/A
+
+n = int(input())
+
+
+solved = 0
+
+for _ in range(n):
+    a,b,c = map(int, input().split())
+    if a + b + c >= 2:
+        solved+=1
+
+print(solved)
