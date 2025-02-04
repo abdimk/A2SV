83e0b186c3b02b1be9648a574c371b261122d6f0
Abdisa Merga <abdisamerga@yahoo.com>
2025-02-04 15:51:46 +0300

    Created 'Subdomain Visit Count' by A2SV Hub

---
+++ 04-Feb-2025/Subdomain Visit Count 239982.py
@@ -0,0 +1,12 @@
+# Problem: Subdomain Visit Count - https://leetcode.com/problems/subdomain-visit-count
+
+class Solution:
+    def subdomainVisits(self, cpdomains):
+        count = collections.Counter()
+        for cd in cpdomains:
+            n, s = cd.split()
+            count[s] += int(n)
+            for i in range(len(s)):
+                if s[i] == '.':
+                    count[s[i + 1:]] += int(n)
+        return ["%d %s" % (count[k], k) for k in count]
