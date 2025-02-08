7496b53a3032612e9de799e6a2783ff2bcd6680c
Abdisa Merga <abdisamerga@yahoo.com>
2025-02-08 11:12:32 +0300

    Created 'Contains Duplicate' by A2SV Hub

---
+++ 08-Feb-2025/Contains Duplicate 251258.py
@@ -0,0 +1,10 @@
+# Problem: Contains Duplicate - https://leetcode.com/problems/contains-duplicate/description/
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = {}
+        for num in nums:
+            if num in seen and seen[num] >= 1:
+                return True
+            seen[num] = seen.get(num, 0) + 1
+        return False
