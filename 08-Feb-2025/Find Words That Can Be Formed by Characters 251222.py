a30654560614486d381552a8af8ffbe794e0b9fa
Abdisa Merga <abdisamerga@yahoo.com>
2025-02-08 09:51:34 +0300

    Created 'Find Words That Can Be Formed by Characters' by A2SV Hub

---
+++ 08-Feb-2025/Find Words That Can Be Formed by Characters 251222.py
@@ -0,0 +1,15 @@
+# Problem: Find Words That Can Be Formed by Characters - https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/description/
+
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        result = 0
+        char_count = Counter(chars)
+
+        for word in words:
+            word_count = Counter(word)
+            if all(word_count[char] <= char_count.get(char, 0) for char in word):
+                result+=len(word)
+
+        return result
+
+
