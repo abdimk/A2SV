a8f690ed4508cc88a4991ddc051b979b61d03a83
Abdisa Merga <abdisamerga@yahoo.com>
2025-02-13 20:35:24 +0300

    Created 'Isomorphic Strings' by A2SV Hub

---
+++ 13-Feb-2025/Isomorphic Strings 263598.py
@@ -0,0 +1,13 @@
+# Problem: Isomorphic Strings - https://leetcode.com/problems/isomorphic-strings/
+
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        map1 = []
+        map2 = []
+        for idx in s:
+            map1.append(s.index(idx))
+        for idx in t:
+            map2.append(t.index(idx))
+        if map1 == map2:
+            return True
+        return False
