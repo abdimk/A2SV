67377092a3e812bba52871c92e25949a676f5733
Abdisa Merga <abdisamerga@yahoo.com>
2025-02-13 20:14:39 +0300

    Created 'Find All Duplicates in an Array' by A2SV Hub

---
+++ 13-Feb-2025/Find All Duplicates in an Array 263573.py
@@ -0,0 +1,9 @@
+# Problem: Find All Duplicates in an Array - https://leetcode.com/problems/find-all-duplicates-in-an-array/description/
+
+class Solution(object):
+    def findDuplicates(self, nums):
+        for i in range(len(nums)):
+            while i != nums[i] - 1 and nums[i] != nums[nums[i]-1]:
+				nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
+
+        return [nums[it] for it in range(len(nums)) if it != nums[it] - 1]
