705853abe71a66552c88cc234ac77223ef50377f
Abdisa Merga <abdisamerga@yahoo.com>
2025-03-13 21:57:47 +0300

    Created 'Remove Element' by A2SV Hub

---
+++ 13-Mar-2025/Remove Element 305741.py
@@ -0,0 +1,11 @@
+# Problem: Remove Element - https://leetcode.com/problems/remove-element/description/
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        index = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[index] = nums[i]
+                index+=1
+
+        return index 
