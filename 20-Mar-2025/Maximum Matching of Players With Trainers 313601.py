387ce24bf9da15f314bdb89eeb3f3af9308d2918
Abdisa Merga <abdisamerga@yahoo.com>
2025-03-20 21:13:05 +0300

    Created 'Maximum Matching of Players With Trainers' by A2SV Hub

---
+++ 20-Mar-2025/Maximum Matching of Players With Trainers 313601.py
@@ -0,0 +1,19 @@
+# Problem: Maximum Matching of Players With Trainers - https://leetcode.com/problems/maximum-matching-of-players-with-trainers/
+
+class Solution:
+    def matchPlayersAndTrainers(self, players: List[int], trainers: List[int]) -> int:
+        p1,p2 = 0,0
+
+        players.sort(), trainers.sort()
+
+        count = 0
+
+        while p1 < len(players) and p2 < len(trainers):
+            if players[p1] < trainers[p2] or players[p1] == trainers[p2]:
+                count+=1
+                p1+=1
+                p2+=1
+            else:
+                p2+=1
+        
+        return count
